3607d4319731d3c1e8e76744a655064119ba4ad4
JBF1962 <36510437+JBF1962@users.noreply.github.com>
2018-02-15 17:44:57 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+let amigo ="Jon Mircha";
+let numero = 2;
+let saludo = `Bienvenido, ${amigo} ${numero * 3} veces`;
+
+let nacimiento = 1962;
+let year = new Date().getFullYear();
+let edad = year - nacimiento;
+
+let hola ="Hola Mundo";
+
+console.log(saludo);
+alert(saludo);
+print(saludo);
